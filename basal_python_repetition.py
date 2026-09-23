ec4c908dea205eb541e60eece1de8d4aa8ac5a7e
frad0001-sys <frad0001@stud.ek.dk>
2026-09-23 15:22:15 +0200

    nyt navn

--- basal_python_repetition.py
+++ basal_python_repetition.py
@@ -1,6 +1,14 @@
-return_hello()
+def return_hello():
+    return "Hello, World!"
+
+
+
+
+"""
+
 average_of_two_numbers(a,b)
 rectangle_area(w,h)
 fahrenheit_to_celsius(f)
 celsius_to_fahrenheit(c)
 echo(text)
+"""
